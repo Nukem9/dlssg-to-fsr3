dde45d418d474525d96954567ae27c29db4276f1
Nukem <Nukem@outlook.com>
2024-01-03 09:49:29 -0500

    Embed version in startup log

--- source/maindll/dllmain.cpp
+++ source/maindll/dllmain.cpp
@@ -41,8 +41,12 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReserved)
 		Util::InitializeLog();
 
 		spdlog::warn("");
-		spdlog::warn("dlssg-to-fsr3 is loaded. AMD FSR 3 Frame Generation will replace Nvidia DLSS-G Frame Generation. Note this does NOT "
-					 "represent a native implementation of AMD's FSR 3.");
+		spdlog::warn(
+			"dlssg-to-fsr3 v{}.{} loaded. AMD FSR 3 Frame Generation will replace Nvidia DLSS-G Frame Generation. Note this does NOT "
+			"represent a native",
+			BUILD_VERSION_MAJOR,
+			BUILD_VERSION_MINOR);
+		spdlog::warn("implementation of AMD's FSR 3.");
 		spdlog::warn("");
 		spdlog::warn("dlssg-to-fsr3 is freely downloadable from https://www.nexusmods.com/site/mods/738?tab=files or "
 					 "https://github.com/Nukem9/dlssg-to-fsr3/releases.");
