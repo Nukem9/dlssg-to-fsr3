ffb2a411b69464678157433dc84eab66b550f3ca
Nukem <Nukem@outlook.com>
2024-08-02 09:47:22 -0400

    Initial gh actions test

--- Make-Release.ps1
+++ Make-Release.ps1
@@ -1,8 +1,12 @@
-# Set up powershell equivalent of vcvarsall.bat
-$vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationpath
+$ErrorActionPreference = "Stop"
 
-Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
-Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments '-arch=x64'
+# Set up powershell equivalent of vcvarsall.bat when CMake/CPack aren't in PATH
+if ((Get-Command "cmake" -ErrorAction SilentlyContinue) -eq $null) {
+    $vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationpath
+
+    Import-Module (Get-ChildItem $vsPath -Recurse -File -Filter Microsoft.VisualStudio.DevShell.dll).FullName
+    Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -DevCmdArguments '-arch=x64'
+}
 
 # Then build with VS
 & cmake --preset final-dtwrapper
