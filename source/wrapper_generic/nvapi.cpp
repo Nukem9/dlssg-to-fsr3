1a659e3d9bff0112136d8c5535198d601227ab16
Nukem <Nukem@outlook.com>
2025-02-05 17:31:28 -0500

    More NvAPI experiments and refactoring

--- source/wrapper_generic/nvapi.cpp
+++ source/wrapper_generic/nvapi.cpp
@@ -10,15 +10,17 @@ enum class NV_STATUS : uint32_t
 enum class NV_INTERFACE : uint32_t
 {
 	GPU_GetArchInfo = 0xD8265D24,
+	D3D12_CreateCubinComputeShaderExV2 = 0x299F5FDC,
+	D3D12_SetFlipConfig = 0xF3148C42,
 	D3D12_SetRawScgPriority = 0x5DB3048A,
 };
 
 struct NV_ARCH_INFO
 {
-	uint32_t Version;
-	uint32_t Architecture;
-	uint32_t Implementation;
-	uint32_t Unknown[1];
+	uint32_t Version;		 // 0
+	uint32_t Architecture;	 // 4
+	uint32_t Implementation; // 8
+	uint32_t Revision;		 // C
 };
 
 struct NV_SCG_PRIORITY_INFO
@@ -33,40 +35,43 @@ struct NV_SCG_PRIORITY_INFO
 	uint32_t Unknown8; // 14
 };
 
-struct NV_D3DKMT_PRIVATE_DRIVER_DATA // nvwgf2umx.dll (546.33)
+struct NV_CREATE_CUBIN_SHADER_INFO
 {
-	uint32_t Header;				 // 0 NVDA
-	char Padding[0xE4];				 // 4
-	uint32_t Architecture;			 // E8
+	uint64_t SizeOfStruct;	// 0
+	uint64_t Unknown1;		// 8
+	void *Unknown2;			// 10
+	void *CubinData;		// 18
+	uint32_t CubinDataSize; // 20
 };
 
 using PfnNvAPI_QueryInterface = void *(__stdcall *)(NV_INTERFACE InterfaceId);
 using PfnNvAPI_GPU_GetArchInfo = NV_STATUS(__stdcall *)(void *GPUHandle, NV_ARCH_INFO *ArchInfo);
+using PfnNvAPI_D3D12_CreateCubinComputeShaderExV2 = NV_STATUS(__stdcall *)(NV_CREATE_CUBIN_SHADER_INFO *CreateInfo);
 
 PfnNvAPI_QueryInterface OriginalNvAPI_QueryInterface = nullptr;
 PfnNvAPI_GPU_GetArchInfo OriginalNvAPI_GPU_GetArchInfo = nullptr;
+PfnNvAPI_D3D12_CreateCubinComputeShaderExV2 OriginalNvAPI_D3D12_CreateCubinComputeShaderExV2 = nullptr;
 decltype(&D3DKMTQueryAdapterInfo) OriginalD3DKMTQueryAdapterInfo = nullptr;
 
 NV_STATUS __stdcall HookedNvAPI_GPU_GetArchInfo(void *GPUHandle, NV_ARCH_INFO *ArchInfo)
 {
-	if (OriginalNvAPI_GPU_GetArchInfo)
+	if (!OriginalNvAPI_GPU_GetArchInfo)
+		return NV_STATUS::Error;
+
+	const auto status = OriginalNvAPI_GPU_GetArchInfo(GPUHandle, ArchInfo);
+
+	if (status == NV_STATUS::Success && ArchInfo && (ArchInfo->Version == 0x10010 || ArchInfo->Version == 0x20010))
 	{
-		const auto status = OriginalNvAPI_GPU_GetArchInfo(GPUHandle, ArchInfo);
-
-		if (status == NV_STATUS::Success && ArchInfo)
+		// if (arch < Ada or arch >= special)
+		if (ArchInfo->Architecture < 0x190 || ArchInfo->Architecture >= 0xE0000000)
 		{
-			// if (arch < ada or arch >= special)
-			if (ArchInfo->Architecture < 0x190 || ArchInfo->Architecture >= 0xE0000000)
-			{
-				ArchInfo->Architecture = 0x190; // Force Ada
-				ArchInfo->Implementation = 4;	// Force GA104
-			}
+			ArchInfo->Architecture = 0x190;	 // Force Ada
+			ArchInfo->Implementation = 4;	 // Force GA104
+			ArchInfo->Revision = 0xFFFFFFFF; // Force ...unknown?
 		}
-
-		return status;
 	}
 
-	return NV_STATUS::Error;
+	return status;
 }
 
 NV_STATUS __stdcall HookedNvAPI_D3D12_SetRawScgPriority(NV_SCG_PRIORITY_INFO *PriorityInfo)
@@ -74,12 +79,58 @@ NV_STATUS __stdcall HookedNvAPI_D3D12_SetRawScgPriority(NV_SCG_PRIORITY_INFO *Pr
 	// SCG or "Simultaneous Compute and Graphics" is their fancy term for async compute. This is a completely
 	// undocumented driver hack used in early versions of sl.dlss_g.dll. Not a single hit on Google.
 	//
-	// nvngx_dlssg.dll feature evaluation somehow prevents crashes. Architecture-specific call? Literally no
-	// clue.
+	// Architecture-specific call. Ada or newer only.
 	//
 	// This function must be stubbed. Otherwise expect undebuggable device removals.
 	return NV_STATUS::Success;
 }
+
+#if 0
+#include "Hooking/Memory.h"
+
+NV_STATUS __stdcall HookedNvAPI_D3D12_CreateCubinComputeShaderExV2(NV_CREATE_CUBIN_SHADER_INFO *CreateInfo)
+{
+	if (!OriginalNvAPI_D3D12_CreateCubinComputeShaderExV2)
+		return NV_STATUS::Error;
+
+	auto result = OriginalNvAPI_D3D12_CreateCubinComputeShaderExV2(CreateInfo);
+
+	if (!CreateInfo)
+		return result;
+
+	if (result == NV_STATUS::Error && CreateInfo && CreateInfo->SizeOfStruct == 0x50)
+	{
+		const bool isFatbin = (CreateInfo->CubinDataSize >= 4) && (memcmp(CreateInfo->CubinData, "\x50\xED\x55\xBA", 4) == 0);
+
+		if (isFatbin)
+		{
+			std::vector cubinData(
+				static_cast<uint8_t *>(CreateInfo->CubinData),
+				static_cast<uint8_t *>(CreateInfo->CubinData) + CreateInfo->CubinDataSize);
+
+			cubinData[0x2C] = 0x59; // sm_89
+
+			while (true)
+			{
+				auto target = Memory::FindPattern(
+					reinterpret_cast<uintptr_t>(cubinData.data()),
+					cubinData.size(),
+					"2E 74 61 72 67 65 74 20 73 6D 5F 31 32 30 0A");
+
+				if (!target)
+					break;
+
+				memcpy(reinterpret_cast<void *>(target), ".target sm_89 \n", 15);
+			}
+
+			CreateInfo->CubinData = cubinData.data();
+			result = OriginalNvAPI_D3D12_CreateCubinComputeShaderExV2(CreateInfo);
+		}
+	}
+
+	return result;
+}
+#endif
 
 void *__stdcall HookedNvAPI_QueryInterface(NV_INTERFACE InterfaceId)
 {
@@ -87,14 +138,24 @@ void *__stdcall HookedNvAPI_QueryInterface(NV_INTERFACE InterfaceId)
 
 	if (result)
 	{
-		if (InterfaceId == NV_INTERFACE::GPU_GetArchInfo)
+		switch (InterfaceId)
 		{
+		case NV_INTERFACE::GPU_GetArchInfo:
 			OriginalNvAPI_GPU_GetArchInfo = static_cast<PfnNvAPI_GPU_GetArchInfo>(result);
 			return &HookedNvAPI_GPU_GetArchInfo;
-		}
 
-		if (InterfaceId == NV_INTERFACE::D3D12_SetRawScgPriority)
+		case NV_INTERFACE::D3D12_SetRawScgPriority:
 			return &HookedNvAPI_D3D12_SetRawScgPriority;
+
+#if 0
+		case NV_INTERFACE::D3D12_CreateCubinComputeShaderExV2:
+			OriginalNvAPI_D3D12_CreateCubinComputeShaderExV2 = static_cast<PfnNvAPI_D3D12_CreateCubinComputeShaderExV2>(result);
+			return &HookedNvAPI_D3D12_CreateCubinComputeShaderExV2;
+
+		case NV_INTERFACE::D3D12_SetFlipConfig:
+			return nullptr;
+#endif
+		}
 	}
 
 	return result;
@@ -107,6 +168,13 @@ NTSTATUS WINAPI HookedD3DKMTQueryAdapterInfo(const D3DKMT_QUERYADAPTERINFO *Info
 
 	if (result == 0 && Info && Info->Type == KMTQAITYPE_UMDRIVERPRIVATE)
 	{
+		struct NV_D3DKMT_PRIVATE_DRIVER_DATA // nvwgf2umx.dll (546.33)
+		{
+			uint32_t Header;				 // 0 NVDA
+			char Padding[0xE4];				 // 4
+			uint32_t Architecture;			 // E8
+		};
+
 		if (Info->pPrivateDriverData && Info->PrivateDriverDataSize >= sizeof(NV_D3DKMT_PRIVATE_DRIVER_DATA))
 		{
 			auto driverData = static_cast<NV_D3DKMT_PRIVATE_DRIVER_DATA *>(Info->pPrivateDriverData);
